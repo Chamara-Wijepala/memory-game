9e43b0710d8f4c73ef038b3cc5a767aba74d471f
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-02 17:40:36 +0530

    Create basic mockup page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React from "react";
+
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <React.Fragment>
+      <header>
+        <button id="ClearScoreButton">Clear Score</button>
+        <div id="ScoreBoard">
+          <p>Score: 0</p>
+          |
+          <p>Best Score: 17</p>
+        </div>
+      </header>
+      <main>
+        <div id="Gameboard">
+          <div className="Card"></div>
+          <div className="Card"></div>
+          <div className="Card"></div>
+        </div>
+      </main>
+    </React.Fragment>
   )
 }
 
