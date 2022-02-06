bb43fd33f083cbbdcc982632b378daccd1111817
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-06 15:11:31 +0530

    Add generateIndices module and rename FetchPokemon module to fetchPokemon

---
+++ src/services/generateIndices.js
@@ -0,0 +1,22 @@
+export default function generateIndices(selectedPokemon) {
+  let indices = [];
+
+  for(let i = 0; i < 3; i++) {
+    let index = Math.floor(Math.random() * 898);
+    indices.push(index);
+  };
+
+  // Returns true if every element in indices matches any of the elements in
+  // selectedPokemon array
+  const noUniqueIndex = indices.every((index) => {
+    return selectedPokemon.some(pokemon => {
+      return pokemon === index;
+    });
+  });
+  
+  if( noUniqueIndex ) {
+    generateIndices(selectedPokemon);
+  } else {
+    return indices;
+  };
+};
