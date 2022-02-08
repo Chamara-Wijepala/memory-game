d1a6b208428fed22a0b20a7ea3194f125c88fbef
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-08 20:56:10 +0530

    Refactor Gameboard component to get api data and pass it to RenderCardDeck sub-component

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,17 +1,47 @@
 import React, { useState, useEffect } from "react";
+import fetchPokemon from "../services/fetchPokemon";
+import generateIndices from '../services/generateIndices'
+import RenderCardDeck from "./RenderCardDeck";
 
 export default function Gameboard() {
-  const [ selectedPokemon, setSelectedPokemon] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(true);
+  const [ selectedPokemon, setSelectedPokemon ] = useState([]);
+  const [ cardDeck, setCardDeck ] = useState([]);
 
+  // useEffect with a value passed to it's dependency array, runs after initial
+  // render and when the value changes. Similar to componentDidUpdate lifecycle
+  useEffect(() => {
+    const indices = generateIndices(selectedPokemon);
+    const promises = [];
+
+    // For each index in indices, fetch a promise and push it to promises array
+    indices.forEach((index) => {
+      promises.push(fetchPokemon(index));
+    });
+
+    // Takes in promises array, then calls setCardDeck and passes in a new
+    // array with all the promises resolved
+    Promise.all(promises).then(pokemon => {
+      setCardDeck(pokemon);
+      setIsLoading(false);
+    });
+  }, [selectedPokemon]);
+
+  // Adds card's id to state when clicked
   function handleClick(event) {
     setSelectedPokemon(prevPokemon => [...prevPokemon, event.target.id])
   };
 
   return (
     <div id="Gameboard">
-      <img className="Card" alt="" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" onClick={handleClick}></img>
-      <img className="Card" alt="" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png" onClick={handleClick}></img>
-      <img className="Card" alt="" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" onClick={handleClick}></img>
+      {
+        isLoading
+          ? 'Loading...'
+          : <RenderCardDeck
+              handleClick={handleClick}
+              cardDeck={cardDeck}
+            ></RenderCardDeck>
+      }
     </div>
   )
 }
