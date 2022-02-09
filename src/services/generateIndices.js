10a7cbc7a4cf4b991aaaad640bf3cb92f4e3c7cf
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-09 14:17:14 +0530

    Fix bug with generateIndices sometimes returning 0

--- src/services/generateIndices.js
+++ src/services/generateIndices.js
@@ -1,8 +1,10 @@
 export default function generateIndices(selectedPokemon) {
   const indices = [];
+  const min = 1;
+  const max = 898;
 
   for(let i = 0; i < 3; i++) {
-    const index = Math.floor(Math.random() * 898);
+    const index = Math.floor(Math.random() * ((max + 1) - min) + min);
     indices.push(index);
   };
 
